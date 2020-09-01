a5f20fac8b53b36c45f82c30995b9e6d4e6138f9
Guneet Singh <guneetnagia24@gmail.com>
2020-09-01 16:01:28 +0530

    jms configuration

--- src/main/java/com/guneet/jms/activemq/config/JmsConfig.java
+++ src/main/java/com/guneet/jms/activemq/config/JmsConfig.java
@@ -1,5 +1,6 @@
 package com.guneet.jms.activemq.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
 import org.springframework.jms.support.converter.MessageConverter;
@@ -7,10 +8,12 @@ import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 public class JmsConfig {
+
+    @Bean
     public MessageConverter messageConverter(){
-        MappingJackson2MessageConverter convertor = new MappingJackson2MessageConverter();
-        convertor.setTargetType(MessageType.TEXT);
-        convertor.setTypeIdPropertyName("_type");
-        return convertor;
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT);
+        converter.setTypeIdPropertyName("_type");
+        return converter;
     }
 }
